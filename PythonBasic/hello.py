97b1e566f0cdf3fde9fc42a9542071011ccaa2a1
vijay <Javascript_user>
2022-01-17 23:25:36 +0530

    Adding python While loop and if else

--- PythonBasic/hello.py
+++ PythonBasic/hello.py
@@ -1,1 +1,13 @@
-print("Hello world");
+
+def protien( num ):
+    if (num <1): 
+        return 1
+    else:
+        return num*protien(num-1)
+    
+i=1
+while i < 6:
+    print("the python")
+    val=int(input("Enter the number"))
+    print(protien(val))
+    i=i+1
